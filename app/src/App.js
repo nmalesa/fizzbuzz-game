3412e942309bf59f486cc4bf91033d8f43f20190
Natalia Malesa <nmalesa@gmail.com>
2022-11-04 15:52:00 -0500

    feat: Add images

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,19 @@ import { createRoot } from "react-dom/client";
 const App = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>FizzBuzz</h1>
+      <div className="img-container">
+        <img
+          className="buzz-img"
+          src={require("../assets/buzz.png")}
+          alt="Illustration of bee"
+        />
+        <img
+          className="fizz-img"
+          src={require("../assets/fizz.jpg")}
+          alt="Illustration of fizzy drink with straw"
+        />
+      </div>
     </div>
   );
 };
