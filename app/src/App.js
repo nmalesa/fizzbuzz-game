6a17f40aafc9ad999cd976dc350778e85aa55aa4
Natalia Malesa <nmalesa@gmail.com>
2022-11-07 16:17:19 -0600

    fix: Debug grid template areas

--- app/src/App.js
+++ app/src/App.js
@@ -27,7 +27,88 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="grid-container">
+      <div className="header">
+        <h1>FizzBuzz</h1>
+        <h2>Save your fizzy beverage from the buzzy bees!</h2>
+      </div>
+      <div className="rules">
+        <p>
+          In FizzBuzz, you are given a random target number. For every number
+          from 1 to the target, multiples of three will print "Fizz", and
+          multiples of five will print "Buzz". Multiples of both three and five
+          will print "FizzBuzz"!
+          <br />
+          The object of the game is to save your fizzy drink by guessing how
+          many times the word "FizzBuzz" will appear.
+        </p>
+      </div>
+      <div className="game">
+        <p className="target">{target}</p>
+        <p className="question">
+          How many times will the word "FizzBuzz" appear?
+        </p>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            getGameData(target);
+            setShowResults(true);
+          }}
+        >
+          <label htmlFor="guess">
+            Enter Your Guess
+            <input
+              id="guess"
+              value={guess}
+              placeholder="Guess"
+              onChange={(e) => setGuess(e.target.value)}
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+        {showResults ? <Results fizzbuzz={fizzbuzz} guess={guess} /> : <></>}
+      </div>
+      <div className="display">
+        <div className="bees-and-bubble-container">
+          {Array.from(Array(fizz)).map((_, idx) => (
+            <FizzBuzzImg
+              key={idx}
+              img={"fizz"}
+              alt={"Illustration of bubble with the word 'FIZZ'"}
+            />
+          ))}
+          {Array.from(Array(buzz)).map((_, idx) => (
+            <FizzBuzzImg
+              key={idx}
+              img={"buzz"}
+              alt={"Illustration of bee with the word 'BUZZ'"}
+            />
+          ))}
+          {Array.from(Array(fizzbuzz)).map((_, idx) => (
+            <FizzBuzzImg
+              key={idx}
+              img={"fizzbuzz"}
+              alt={"Illustration of bubble and bee with the word 'FIZZBUZZ'"}
+            />
+          ))}
+        </div>
+        <img
+          className="drink-img"
+          src={require("./assets/drink.jpg")}
+          alt="Illustration of fizzy drink with straw"
+        />
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App tab="home" />);
+
+{
+  /* <div>
       <div className="header">
         <h1>FizzBuzz</h1>
         <h2>Save your fizzy beverage from the buzzy bees!</h2>
@@ -97,11 +178,5 @@ const App = () => {
           alt="Illustration of fizzy drink with straw"
         />
       </div>
-      {showResults ? <Results fizzbuzz={fizzbuzz} guess={guess} /> : <></>}
-    </div>
-  );
-};
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App tab="home" />);
+      {showResults ? <Results fizzbuzz={fizzbuzz} guess={guess} /> : <></>} */
+}
